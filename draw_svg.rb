4a1d971168fd89ea77f7993955a01fe61c608c3d
nicola <niquola@gmail.com>
2013-02-26 00:42:33 +0400

    use classes

--- draw_svg.rb
+++ draw_svg.rb
@@ -1,22 +1,46 @@
-require 'vangog'
+require 'rubygems'
+require File.dirname(__FILE__) + '/vangog'
+require 'erb'
 
 File.open('test.svg', 'w') do |f|
   now = Time.now
-  max_widht = 1200
+  max_widht = 3000
   max_height = 800
   ws = hs = 20
   mpt = pt(ws,max_height/2)
-  d = (max_widht - 2*ws)/5
+  d = (max_widht - 2*ws)/50
 
 
   body = []
   body<< arrow(mpt,'black', max_widht - 2*ws,2*hs)
+
+  colors = %[green orange red yellow gray]
+  30.times do |i|
+    body<< trapecia(mpt + pt(i*200, 0), 190, 'green')
+    body<< trapecia(mpt + pt(i*200, 40), 100, 'orange', true)
+  end
+
   body<< arrow(mpt + pt(0,-ws/2), 'gray', max_widht - 2*ws,hs/2)
 
-  6.times do |i|
+  50.times do |i|
     body<< flag(mpt + pt(i*d, -ws/4), max_height/3, ws/4, 'red')
   end
 
-  f<< %Q[<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> #{body.join('')} </svg>]
+  @svg = %Q[<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> #{body.join('')} </svg>]
   puts "#{Time.now - now} s"
+
+  f<< ERB.new(DATA.read).result(binding)
 end
+
+__END__
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+<style type="text/css" >
+<![CDATA[
+  .flag {
+    stroke: #006600;
+    fill:   #00cc00;
+  }
+]]>
+</style>
+<%= body.join(' ') %>
+</svg>
--- vangog.rb
+++ vangog.rb
@@ -27,26 +27,34 @@ def pt(x,y)
   Pt.new(x,y)
 end
 
-def path(p,color, *pts)
+def path(p,cls, *pts)
   pth = pts.map{|pt| (pt + p).to_s }.join(" ")
-  %Q[<polygon points="#{pth}" fill="#{color}"/>]
+  %Q[<polygon points="#{pth}" class="#{cls}"/>]
 end
 
-def arrow(p,color,w,h)
-  path(p,color, pt(0,0),pt(w,0),pt(w+h,h/2),pt(w,h),pt(0,h))
+def arrow(p,cls,w,h)
+  path(p,cls, pt(0,0),pt(w,0),pt(w+h,h/2),pt(w,h),pt(0,h))
 end
 
-def circle(p,r)
-  %Q[<circle cx="#{p.x}" cy="#{p.y}" r="#{r}" fill="red"/>]
+def circle(p, r, cls='')
+  %Q[<circle cx="#{p.x}" cy="#{p.y}" r="#{r}" class="#{cls}"/>]
 end
 
-def line(p1,p2)
-  %Q[<line x1="#{p1.x}" y1="#{p1.y}" x2="#{p2.x}" y2="#{p2.y}" style="stroke-width:2; stroke:rgb(255,0,0);"/>]
+def line(p1, p2, cls='')
+  %Q[<line x1="#{p1.x}" y1="#{p1.y}" x2="#{p2.x}" y2="#{p2.y}" class="#{cls}"/>]
 end
 
-def flag(p,h,r,color)
+def flag(p,h,r,cls)
+  cls = "#{cls} flag"
   up = p + pt(0,-h)
-  res = circle(p,r)
-  res<< line(p, up)
-  res<< path(up,color,pt(0,0), pt(20,0), pt(10,10), pt(20, 20),pt(0, 20), pt(0,0))
+  res = circle(p,r, cls)
+  res<< line(p, up, cls)
+  res<< path(up,cls,pt(0,0), pt(20,0), pt(10,10), pt(20, 20),pt(0, 20), pt(0,0))
+end
+
+def trapecia(p,w, cls, down = false)
+  h = 40
+  h = -h if down
+  d = 20
+  path(p, cls, pt(d, -h), pt(w-d,-h), pt(w, 0), pt(0,0))
 end
